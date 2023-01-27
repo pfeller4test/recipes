da91f2f6a48080c20f2645294945c7860dd24433
pfeller4test <pfeller@outlook.com>
2023-01-27 14:13:50 -0800

    created a thing for a bulleted list

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+class BulletList extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>Bullet List for{this.props.name}</h1>
+            <ul>
+                <li>Recipe One</li>
+                <li>Recipe Two</li>
+                <li>Recipe Three</li>
+            </ul>
+        </div>
+        );
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BulletList />);
